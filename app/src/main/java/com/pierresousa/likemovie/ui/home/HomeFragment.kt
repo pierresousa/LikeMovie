e372f12bca39333b595a3bc9ac31cbefbc3d63e8
pierresousa <pierrevssousa@gmail.com>
2024-01-03 18:52:39 -0300

    fix: remove coil reference

--- app/src/main/java/com/pierresousa/likemovie/ui/home/HomeFragment.kt
+++ app/src/main/java/com/pierresousa/likemovie/ui/home/HomeFragment.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import coil.load
 import com.pierresousa.likemovie.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
